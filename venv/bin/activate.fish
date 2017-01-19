a627659ace957461c42982ad97ad04d7dcd71ba1
Christina Harlow <cmharlow@gmail.com>
2017-01-19 12:27:27 -0500

    hiding venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/brighid/Projects/data_qa/venv"
+set -gx VIRTUAL_ENV "/Users/Christina/Projects/data-qa/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
